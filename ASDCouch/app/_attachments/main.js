1ad39ccdf6360d046e93ddab95531ab49931c260
Boyd Tiffin <mokono@fullsail.edu>
2013-12-19 16:43:21 -0600

    Week 4 Work

--- ASDCouch/app/_attachments/main.js
+++ ASDCouch/app/_attachments/main.js
@@ -2,255 +2,272 @@
 //1312 - ASDI
 
 // This checks to see if the page has loaded, then executes the code within
-$(function(){
-
-
-// Pageinit function runs the code inside when that page is called. This is due to jQM and how pages are coded within jQM.
-
 // HOME page and specific functions to run on load
-	$('#home').on('pageinit', function(){
-		//code needed for home page goes here
-	});	
-
-// ADDITEM page and specific functions runnign on load	
-	$('#additem').on('pageinit', function(){
-		//any other code needed for addItem page goes here
-		var myForm = $('#addService'),
-			addServiceFormErrorsLink = $('#addServiceFormErrorsLink');
-		    myForm.validate({
-			    invalidHandler: function(myForm, validator){
-				    addServiceFormErrorsLink.click();
-		// THis is pulling the added jQM label of required and does not revalidate each time the form is submitted
-				    var html = '';
-				    for(var key in validator.submitted){
-					    var label = $('label[for^="'+ key + '"]').not('[generated]');
-					    var legend = label.closest('fieldset').find('.ui-controlgroup-label');
-					    var fieldName = legend.length ? legend.text() : label.text();
-					    html += '<li>' + fieldName +'</li>';
-				    };
-				    $("#addServiceFormErrors ul").html(html);
-			    },
-			    submitHandler: function(){
-				    var data = myForm.serializeArray();
-				    var key = $('#hiddenKey').attr('data-key');
-				    if(!key){
-					    storeItem()
-				    }else{
-					    var key = $('#hiddenKey').attr('data-key');
-					    storeItem(key)
-				    }
-			    }
-		    });
+$(document).on('pageinit', '#viewAll', function(){
+	$.couch.db("development").view("asdproject/bathroom", {
+		success: function(data){
+			$('<li data-role="list-divider">Bathroom</li>')
+					.appendTo('#allRecordsParent');
+				$.each(data.rows, function(index, sr){
+					var id = sr.id;
+					var srTitle = sr.value.Title;
+					$('#allRecordsParent')
+						.append(
+							$('<li>')
+								.append(
+									$('<a>')
+										.attr({
+											href: "records.html?id=" + id,
+										})
+										.text(srTitle)
+								)
+						);
+				});
+			$('#allRecordsParent').listview('refresh');
+		}
 	});
-
-// OPTIONS page functions that need to run specific to that page
-	$('#options').on('pageinit', function(){
-		$("#butClearAll").on("click", function(){
-			//Prompt user that this is unrecoverable and get confirmation
-			var x = confirm("Clear All Data? This is not reversible");
-			if(x){
-				// User selected ok, so run the function
-				deleteAll();
-			}else{
-				//User selected CANCEL, prompt and move on
-				alert("Data has not been cleared! That was close...");
-			}
-			
-		});
-	});	
-	
-// VIEW ALL RECORDS PAGE
-	$('#viewAll').on('pageinit', function(){
-		// Display items that are currently in the couch database
-		// Add a header line for the category first
-		$.ajax({
-			"url": "_view/mechanical",
-			"dataType": "json",
-			"success": function(data){
-				$('<li data-role="list-divider">Mechanical</li>')
+	$.couch.db("development").view("asdproject/mechanical", {
+		success: function(data){
+			$('<li data-role="list-divider">Mechanical</li>')
 					.appendTo('#allRecordsParent');
 				$.each(data.rows, function(index, sr){
 					var id = sr.id;
 					var srTitle = sr.value.Title;
-					var srDate = sr.value.srDate;
-					var srDesc = sr.value.srDesc;
-					var srExpires = sr.value.srExpires;
-					var srName = sr.value.srName;
-					var srPhone = sr.value.srPhone;
-					var srWarranty = sr.value.srWarranty;
 					$('#allRecordsParent')
 						.append(
 							$('<li>')
 								.append(
 									$('<a>')
 										.attr({
-											href: "#",
-											id: id
+											href: "records.html?id=" + id,
 										})
 										.text(srTitle)
 								)
 						);
 				});
-				$('#allRecordsParent').listview('refresh');
-			}
-		});
-		$.ajax({
-			"url": "_view/bathroom",
-			"dataType": "json",
-			"success": function(data){
-				$('<li data-role="list-divider">Bathroom</li>')
-					.appendTo('#allRecordsParent');
-				$.each(data.rows, function(index, sr){
-					var id = sr.id;
-					var srTitle = sr.value.Title;
-					var srDate = sr.value.srDate;
-					var srDesc = sr.value.srDesc;
-					var srExpires = sr.value.srExpires;
-					var srName = sr.value.srName;
-					var srPhone = sr.value.srPhone;
-					var srWarranty = sr.value.srWarranty;
-					$('#allRecordsParent')
-						.append(
-							$('<li>')
-								.append(
-									$('<a>')
-										.attr({
-											href: "#",
-											id: id
-										})
-										.text(srTitle)
-								)
-						);
-				});
-				$('#allRecordsParent').listview('refresh');
-			}
-		});
-		
-/*		This is the old way when it pulled from local storage
-		for(var i=0, j=localStorage.length; i<j; i++){
-			var key = localStorage.key(i);
-			var value = localStorage.getItem(key);
-			// String to Obj
-			var obj = JSON.parse(value);
-			for(var d in obj){
-				$('<li>' + obj[d][0]+" " +obj[d][1] + '</li>')
-					.appendTo("#allRecordsParent");
-			}
-			editDeleteLinks(key)
-			$('<br /><br />')
-				.appendTo("#allRecordsParent");
+			$('#allRecordsParent').listview('refresh');
 		}
-*/
-	// AJAX Requests Start Here
-		// JSON FILE
-		$("#loadJSON").on("click", function(){
-			$.getJSON( "js/json.json", function(data) {
-			}).done(function(data){
-				// Object now exists called data, this is my JSON Object
-				for(var key in data){
-					var obj = data[key];
-					for(var m in obj){
-						$('<li>' + m +" " +obj[m] + '</li>')
-							.appendTo("#allRecordsParent");
-					}
-					$('<br />')
-					.appendTo("#allRecordsParent");
-					$("#allRecordsParent").listview('refresh');
-				}
-			});
-		});
-		
-		//XML FILE
-		$("#loadXML").on("click", function(){
-			$.get("js/xml.xml", function(data){
-				array = $(data).find("Record");
-				for(x=0, y=array.length; x<y; x++){
-					var loopV = array[x];
-					var values = $(loopV).text();
-					$('<li>' + values + '</li><br />')
-						.appendTo("#allRecordsParent");
-				}
-			})
-		});
-	});	
-// Function to create the edit and delete links for each record
-	var editDeleteLinks = function(key){
-		$('<a href="#additem">Edit Item</a>')
-			.appendTo("#allRecordsParent")
-			.attr('data-key', key )
-			.on("click", function(){
-				editItem(key);
-			});
-		//add spaces between edit and delete
-		$('<span class="spacer"></span>')
-			.html("&nbsp;&nbsp;&nbsp;")
-			.appendTo("#allRecordsParent");
-		$('<a href="#" >Delete Item</a>')
-			.appendTo("#allRecordsParent")
-			.attr('data-key', key )
-			.on("click", function(){
-				deleteItem(key);
-			});
+	});
+});
+
+var urlVars = function(urlData){
+//	var urlData = $(this).data("url");
+	var urlParts = urlData.split('?');
+	var urlPairs = urlParts[1].split('&');
+	var urlValues = {};
+	for (var pair in urlPairs) {
+		var keyValue = urlPairs[pair].split('=');
+		var key = decodeURIComponent(keyValue[0]);
+		var value = decodeURIComponent(keyValue[1]);
+		urlValues[key] = value;
 	}
-	
-	
-// Below are functions that can be called from any page, everything here needs to be stored as variables so that it is not run everytime a page loads
-
-	var storeItem = function(key){
-		if(!key){
-			var id = Math.floor(Math.random()*100000001);
-		}else{
-			id = key;
+	return urlValues;
+};
+// Individual record display page
+$(document).on('pageinit', '#records', function(){
+	var id = "";
+	var test = urlVars($(this).data("url"));
+	newId = test["id"];
+	$.couch.db("development").openDoc(newId, {
+		success: function(data){
+			//display the item specific data
+			var id = data._id;
+			var rev = data._rev;
+			var srTitle = data.Title;
+			var srDate = data.srDate;
+			var srDesc = data.srDesc;
+			var srExpires = data.srExpires;
+			var srName = data.srName;
+			var srPhone = data.srPhone;
+			var srWarranty = data.srWarranty;
+			var deleteRecord = {
+					_id:id , 
+					_rev: rev
+			};
+			// Display the individual record data as desired
+			// Breaking these out individually for future formatting
+			$("#recordsParent")
+				.append($('<li>')
+					.text("Title: "+srTitle)
+				)
+			.append($('<li>')
+					.text("Date of Service: "+srDate)
+				)
+			.append($('<li>')
+					.text("Servicer Name: "+srName)
+				)
+			.append($('<li>')
+					.text("Servicer Phone Number: "+srPhone)
+				)
+			.append($('<li>')
+					.text("Full Description of Work: "+srDesc)
+				);
+		// Does this item have a warranty with it? Display appro answer.
+			if(srWarranty === false){
+				$("#recordsParent")
+				.append($('<li>')
+						.text("No Warranty!")
+					)
+			}else{
+				$("#recordsParent")
+				.append($('<li>')
+						.text("Warranty expires on: "+srExpires)
+			)};
+		// Create delete Link
+			$("#recordsParent")
+			.append("<a href=\"#\" id=\"deleteRecord\" data-role=\"button\">Delete Record</a>");
+			$("#deleteRecord").on("click", function(){
+				//Prompt user that this is unrecoverable and get confirmation
+				var x = confirm("Delete This Record? This is not reversible");
+				if(x){
+					// User selected ok, so run the function
+					$.couch.db("development").removeDoc(deleteRecord, {
+					     success: function(data) {
+					         alert("Record has been removed");
+					    },
+					    error: function(status) {
+					        alert("The record was not removed, try again later");
+					    }
+					});
+				}else{
+					//User selected CANCEL, prompt and move on
+					alert("Record has not been cleared! That was close...");
+				}
+				
+			});
+		// Create Edit Link
+			$("#recordsParent")
+			.append("<a data-role=\"button\" href=\"edititem.html?id=" + id + "\">Edit Record</a>");
+			// Refresh the view so it looks pretty
+			$("#recordsParent").listview('refresh');
 		}
-		// Get all form data in an Object
-		var idea = {};
-			idea.scTitle = ["Title:", $('#scTitle').val()];
-			idea.scDate = ["Service Date:", $('#scDate').val()];
-			idea.scCompany = ["Servicer Name:", $('#scCompany').val()];
-			idea.scPhone = ["Servicer Phone:", $('#scPhone').val()];
-			idea.scWarranty = ["Servicer Warranty?:", $('#scWarranty').val()];
-			idea.scWarDate = ["Warranty Expires:", $('#scWarDate').val()];
-			idea.scDesc = ["Description:", $('#scDesc').val()];
-			// Add Radio Check to get value
-		localStorage.setItem(id, JSON.stringify(idea));
-		alert("Service Record Logged!");
-		window.location="#home";
-	};
+	});
+});
 	
-	var editItem = function(key){
-		console.log(JSON.parse(localStorage.getItem(key)));
-		var itemValue = JSON.parse(localStorage.getItem(key));
-		$('#scTitle').val(itemValue.scTitle[1]);
-		$('#scDate').val(itemValue.scDate[1]);
-		$('#scCompany').val(itemValue.scCompany[1]);
-		$('#scPhone').val(itemValue.scPhone[1]);
-		$('#scWarranty').val(itemValue.scWarranty[1]);
-		$('#scWarDate').val(itemValue.scWarDate[1]);
-		$('#scDesc').val(itemValue.scDesc[1]);
-		// Change button from Add Record to Edit Record
-		$('#subService').val('Edit Record');
-		$('#hiddenKey')
-			.attr('data-key', key);
-	};
-	
-	var deleteAll = function(){
-		if(localStorage.length === 0){
-			alert("There is no data to clear.");
-		}else{
-			localStorage.clear();
-			alert("All data has been cleared!");
-			window.location.reload();
-			return false;
+// Add New Record to CouchDB
+$(document).on('pageinit', '#additem', function(){
+	//any other code needed for addItem page goes here	
+	var myForm = $('#addService'),
+		addServiceFormErrorsLink = $('#addServiceFormErrorsLink');
+	    myForm.validate({
+		    invalidHandler: function(myForm, validator){
+			    addServiceFormErrorsLink.click();
+	// THis is pulling the added jQM label of required and does not revalidate each time the form is submitted
+			    var html = '';
+			    for(var key in validator.submitted){
+				    var label = $('label[for^="'+ key + '"]').not('[generated]');
+				    var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+				    var fieldName = legend.length ? legend.text() : label.text();
+				    html += '<li>' + fieldName +'</li>';
+			    };
+			    $("#addServiceFormErrors ul").html(html);
+		    },
+		    submitHandler: function(){
+			    var data = myForm.serializeArray();
+			    var key = $('#hiddenKey').attr('data-key');
+			    // Set the object variable for CouchDB
+			    var warr = $('#scWarranty').val();
+			    if(warr === "false"){
+			    	warr = false;
+			    };
+			    var record = {
+			    		Title: $('#scTitle').val(),
+			    		srName: $('#scCompany').val(),
+			    		srDate: $('#scDate').val(),
+			    		srWarranty: warr,
+			    		srPhone: $('#scPhone').val(),
+			    		srExpires: $('#scWarDate').val(),
+			    		srDesc: $('#scDesc').val(),
+			    		type: $('#type').val()
+			    };
+			    // Add item to DB
+			    $.couch.db("development").saveDoc(record, {
+			        success: function(data) {
+			            alert("New Record Saved");
+			        },
+			        error: function(status) {
+			            alert("Record not Saved");
+			        }
+			    });
+		    }
+	    });
+});
+// Edit Item Page
+$(document).on('pageinit', '#edititem', function(){
+	//If this is a edit record, check to see if the value is in the URL
+	var test = urlVars($(this).data("url"));
+	newId = test["id"];
+	$.couch.db("development").openDoc(newId, {
+		success: function(data){
+			var parse = JSON.parse(data);
+			//Get the item specific data from Couch
+			var id = parse._id;
+			var rev = parse._rev;
+			var editTitle = parse.Title;
+			var editDate = parse.srDate;
+			var editDesc = parse.srDesc;
+			var editExpires = parse.srExpires;
+			var editName = parse.srName;
+			var editPhone = parse.srPhone;
+			var editWarranty = parse.srWarranty;
+			var editType = parse.type;
+			// Set the values of the fields on the page
+			$("#scTitle").val(editTitle);
+    		$('#scCompany').val(editName);
+    		$('#scDate').val(editDate);
+    		$('#scWarranty').val(editWarranty);
+    		$('#scPhone').val(editPhone);
+    		$('#scWarDate').val(editExpires);
+    		$('#scDesc').val(editDesc);
+    		$('#type').val(editType);
+    		alert(editType);
 		}
-	};
-	
-	var deleteItem = function(key){
-		var ask = confirm("Are you sure you want to delete this idea?");
-		if(ask){
-			localStorage.removeItem(key);
-			window.location.reload();
-		}else{
-			alert("Idea was not deleted, now get to work!")
-		}
-	};
+	});
+	//any other code needed for addItem page goes here	
+	var myForm = $('#editService'),
+		addServiceFormErrorsLink = $('#addServiceFormErrorsLink');
+	    myForm.validate({
+		    invalidHandler: function(myForm, validator){
+			    addServiceFormErrorsLink.click();
+	// THis is pulling the added jQM label of required and does not revalidate each time the form is submitted
+			    var html = '';
+			    for(var key in validator.submitted){
+				    var label = $('label[for^="'+ key + '"]').not('[generated]');
+				    var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+				    var fieldName = legend.length ? legend.text() : label.text();
+				    html += '<li>' + fieldName +'</li>';
+			    };
+			    $("#addServiceFormErrors ul").html(html);
+		    },
+		    submitHandler: function(){
+			    var data = myForm.serializeArray();
+			    var key = $('#hiddenKey').attr('data-key');
+			    // Set the object variable for CouchDB
+			    var warr = $('#scWarranty').val();
+			    if(warr === "false"){
+			    	warr = false;
+			    };
+			    var record = {
+			    		_id: id,
+			    		_rev: rev,
+			    		Title: $('#scTitle').val(),
+			    		srName: $('#scCompany').val(),
+			    		srDate: $('#scDate').val(),
+			    		srWarranty: warr,
+			    		srPhone: $('#scPhone').val(),
+			    		srExpires: $('#scWarDate').val(),
+			    		srDesc: $('#scDesc').val(),
+			    		type: $('#type').val()
+			    };
+			    // Add item to DB
+			    $.couch.db("development").saveDoc(record, {
+			        success: function(data) {
+			            alert("New Record Saved");
+			        },
+			        error: function(status) {
+			            alert("Record not Saved");
+			        }
+			    });
+		    }
+	    });
 });
---
+++ ASDCouch/app/_attachments/oldMain.js
@@ -0,0 +1,242 @@
+// ADDITEM page and specific functions runnign on load	
+	$('#additem').on('pageinit', function(){
+		//any other code needed for addItem page goes here
+		var myForm = $('#addService'),
+			addServiceFormErrorsLink = $('#addServiceFormErrorsLink');
+		    myForm.validate({
+			    invalidHandler: function(myForm, validator){
+				    addServiceFormErrorsLink.click();
+		// THis is pulling the added jQM label of required and does not revalidate each time the form is submitted
+				    var html = '';
+				    for(var key in validator.submitted){
+					    var label = $('label[for^="'+ key + '"]').not('[generated]');
+					    var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+					    var fieldName = legend.length ? legend.text() : label.text();
+					    html += '<li>' + fieldName +'</li>';
+				    };
+				    $("#addServiceFormErrors ul").html(html);
+			    },
+			    submitHandler: function(){
+				    var data = myForm.serializeArray();
+				    var key = $('#hiddenKey').attr('data-key');
+				    if(!key){
+					    storeItem()
+				    }else{
+					    var key = $('#hiddenKey').attr('data-key');
+					    storeItem(key)
+				    }
+			    }
+		    });
+	});
+
+// OPTIONS page functions that need to run specific to that page
+	$('#options').on('pageinit', function(){
+		$("#butClearAll").on("click", function(){
+			//Prompt user that this is unrecoverable and get confirmation
+			var x = confirm("Clear All Data? This is not reversible");
+			if(x){
+				// User selected ok, so run the function
+				deleteAll();
+			}else{
+				//User selected CANCEL, prompt and move on
+				alert("Data has not been cleared! That was close...");
+			}
+			
+		});
+	});	
+	
+// VIEW ALL RECORDS PAGE
+	$('#test').on('pageinit', function(){
+		// Display items that are currently in the couch database
+		// Add a header line for the category first
+		$.ajax({
+			"url": "_view/mechanical",
+			"dataType": "json",
+			"success": function(data){
+				$('<li data-role="list-divider">Mechanical</li>')
+					.appendTo('#allRecordsParent');
+				$.each(data.rows, function(index, sr){
+					var id = sr.id;
+					var srTitle = sr.value.Title;
+					var srDate = sr.value.srDate;
+					var srDesc = sr.value.srDesc;
+					var srExpires = sr.value.srExpires;
+					var srName = sr.value.srName;
+					var srPhone = sr.value.srPhone;
+					var srWarranty = sr.value.srWarranty;
+					$('#allRecordsParent')
+						.append(
+							$('<li>')
+								.append(
+									$('<a>')
+										.attr({
+											href: "#",
+											id: id
+										})
+										.text(srTitle)
+								)
+						);
+				});
+				$('#allRecordsParent').listview('refresh');
+			}
+		});
+		$.ajax({
+			"url": "_view/bathroom",
+			"dataType": "json",
+			"success": function(data){
+				$('<li data-role="list-divider">Bathroom</li>')
+					.appendTo('#allRecordsParent');
+				$.each(data.rows, function(index, sr){
+					var id = sr.id;
+					var srTitle = sr.value.Title;
+					var srDate = sr.value.srDate;
+					var srDesc = sr.value.srDesc;
+					var srExpires = sr.value.srExpires;
+					var srName = sr.value.srName;
+					var srPhone = sr.value.srPhone;
+					var srWarranty = sr.value.srWarranty;
+					$('#allRecordsParent')
+						.append(
+							$('<li>')
+								.append(
+									$('<a>')
+										.attr({
+											href: "#",
+											id: id
+										})
+										.text(srTitle)
+								)
+						);
+				});
+				$('#allRecordsParent').listview('refresh');
+			}
+		});
+		
+/*		This is the old way when it pulled from local storage
+		for(var i=0, j=localStorage.length; i<j; i++){
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			// String to Obj
+			var obj = JSON.parse(value);
+			for(var d in obj){
+				$('<li>' + obj[d][0]+" " +obj[d][1] + '</li>')
+					.appendTo("#allRecordsParent");
+			}
+			editDeleteLinks(key)
+			$('<br /><br />')
+				.appendTo("#allRecordsParent");
+		}
+*/
+	// AJAX Requests Start Here
+		// JSON FILE
+		$("#loadJSON").on("click", function(){
+			$.getJSON( "js/json.json", function(data) {
+			}).done(function(data){
+				// Object now exists called data, this is my JSON Object
+				for(var key in data){
+					var obj = data[key];
+					for(var m in obj){
+						$('<li>' + m +" " +obj[m] + '</li>')
+							.appendTo("#allRecordsParent");
+					}
+					$('<br />')
+					.appendTo("#allRecordsParent");
+					$("#allRecordsParent").listview('refresh');
+				}
+			});
+		});
+		
+		//XML FILE
+		$("#loadXML").on("click", function(){
+			$.get("js/xml.xml", function(data){
+				array = $(data).find("Record");
+				for(x=0, y=array.length; x<y; x++){
+					var loopV = array[x];
+					var values = $(loopV).text();
+					$('<li>' + values + '</li><br />')
+						.appendTo("#allRecordsParent");
+				}
+			})
+		});
+	});	
+// Function to create the edit and delete links for each record
+	var editDeleteLinks = function(key){
+		$('<a href="#additem">Edit Item</a>')
+			.appendTo("#allRecordsParent")
+			.attr('data-key', key )
+			.on("click", function(){
+				editItem(key);
+			});
+		//add spaces between edit and delete
+		$('<span class="spacer"></span>')
+			.html("&nbsp;&nbsp;&nbsp;")
+			.appendTo("#allRecordsParent");
+		$('<a href="#" >Delete Item</a>')
+			.appendTo("#allRecordsParent")
+			.attr('data-key', key )
+			.on("click", function(){
+				deleteItem(key);
+			});
+	}
+	
+	
+// Below are functions that can be called from any page, everything here needs to be stored as variables so that it is not run everytime a page loads
+
+	var storeItem = function(key){
+		if(!key){
+			var id = Math.floor(Math.random()*100000001);
+		}else{
+			id = key;
+		}
+		// Get all form data in an Object
+		var idea = {};
+			idea.scTitle = ["Title:", $('#scTitle').val()];
+			idea.scDate = ["Service Date:", $('#scDate').val()];
+			idea.scCompany = ["Servicer Name:", $('#scCompany').val()];
+			idea.scPhone = ["Servicer Phone:", $('#scPhone').val()];
+			idea.scWarranty = ["Servicer Warranty?:", $('#scWarranty').val()];
+			idea.scWarDate = ["Warranty Expires:", $('#scWarDate').val()];
+			idea.scDesc = ["Description:", $('#scDesc').val()];
+			// Add Radio Check to get value
+		localStorage.setItem(id, JSON.stringify(idea));
+		alert("Service Record Logged!");
+		window.location="#home";
+	};
+	
+	var editItem = function(key){
+		console.log(JSON.parse(localStorage.getItem(key)));
+		var itemValue = JSON.parse(localStorage.getItem(key));
+		$('#scTitle').val(itemValue.scTitle[1]);
+		$('#scDate').val(itemValue.scDate[1]);
+		$('#scCompany').val(itemValue.scCompany[1]);
+		$('#scPhone').val(itemValue.scPhone[1]);
+		$('#scWarranty').val(itemValue.scWarranty[1]);
+		$('#scWarDate').val(itemValue.scWarDate[1]);
+		$('#scDesc').val(itemValue.scDesc[1]);
+		// Change button from Add Record to Edit Record
+		$('#subService').val('Edit Record');
+		$('#hiddenKey')
+			.attr('data-key', key);
+	};
+	
+	var deleteAll = function(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear.");
+		}else{
+			localStorage.clear();
+			alert("All data has been cleared!");
+			window.location.reload();
+			return false;
+		}
+	};
+	
+	var deleteItem = function(key){
+		var ask = confirm("Are you sure you want to delete this idea?");
+		if(ask){
+			localStorage.removeItem(key);
+			window.location.reload();
+		}else{
+			alert("Idea was not deleted, now get to work!")
+		}
+	};
+});
